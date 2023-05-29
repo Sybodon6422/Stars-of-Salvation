f1cba123971d17f70a9707c87b141e22fe2a7784
Sybodon6422 <58808836+Sybodon6422@users.noreply.github.com>
2023-05-29 17:02:33 -0400

    Pathfinding
    Added pathfinding and more basics to the building mechanics

---
+++ Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Resource", menuName = "Gamestuff/New Resource")]public class Resource : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
